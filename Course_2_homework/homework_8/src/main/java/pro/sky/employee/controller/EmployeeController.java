8339a7955a555e948dbf1f4bd48b2dd9749b6e78
Stepan <stepan-morozov-1980@mail.ru>
2023-04-11 20:29:47 +0400

    ...

--- Course_2_homework/homework_8/src/main/java/pro/sky/employee/controller/EmployeeController.java
+++ Course_2_homework/homework_8/src/main/java/pro/sky/employee/controller/EmployeeController.java
@@ -1,11 +1,15 @@
 package pro.sky.employee.controller;
 
+import pro.sky.employee.Employee;
 import pro.sky.employee.service.EmployeeService;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/departments")
@@ -40,20 +44,16 @@ public class EmployeeController {
         }
     }
 
-    @GetMapping(path = "/all")
-    @NotNull
+    @GetMapping ("/all")
     public String allInDepartment
             (@RequestParam(value = "departmentId", required = false) Integer department) {
-        if (department < 1 || department > 5) {
+        if (department == null){
+            return employeeService.all();
+        } else if (department < 1 || department > 5) {
             return "У нас имеется только 5 отделов";
         } else {
-            String employeeMin = employeeService.allInDepartment(department);
-            return employeeMin.isEmpty() ? "В этом отделе нету сотрудников" : employeeMin;
+            String result = employeeService.allInDepartment(department);
+            return result.isEmpty() ? "В этом отделе нету сотрудников" : result;
         }
-    }
-
-    @GetMapping(path = "/all")
-    public String all() {
-        return employeeService.all();
     }
 }
--- Course_2_homework/homework_8/src/main/java/pro/sky/employee/service/EmployeeService.java
+++ Course_2_homework/homework_8/src/main/java/pro/sky/employee/service/EmployeeService.java
@@ -3,9 +3,7 @@ package pro.sky.employee.service;
 import pro.sky.employee.Employee;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService {
@@ -31,12 +29,18 @@ public class EmployeeService {
     public String allInDepartment(int department) {
         return employees.stream()
                 .filter(div -> div.getDivision() == department)
-                .toString();
+                .toList().toString();
     }
 
     public String all() {
-        return employees.stream()
-                .sorted(Comparator.comparing(Employee::getDivision))
-                .toString();
+        int maxDepartment = employees.stream()
+                .max(Comparator.comparing(Employee::getDivision))
+                .get().getDivision();
+        Map<String, List<Employee>> result = new HashMap<>();
+        for (int i = 1; i <= maxDepartment; i++) {
+            int iEx = i;
+            result.put("Сотрудники " + i + " отдела", employees.stream().filter(x -> x.getDivision() == iEx).toList());
+        }
+        return result.toString();
     }
 }
