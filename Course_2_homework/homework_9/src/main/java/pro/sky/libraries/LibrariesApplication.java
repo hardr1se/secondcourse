df19629648b83c446ed21b447c1182dad3ad9f39
Stepan <stepan-morozov-1980@mail.ru>
2023-04-26 11:24:04 +0400

    ...

---
+++ Course_2_homework/homework_9/src/main/java/pro/sky/libraries/LibrariesApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariesApplication {
+    public LibrariesApplication() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrariesApplication.class, args);
+    }
+}
---
+++ Course_2_homework/homework_9/src/main/java/pro/sky/libraries/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,11 @@
+package pro.sky.libraries.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
